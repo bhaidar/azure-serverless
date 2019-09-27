67dd066d8efcad0946df8ef3914d0bf282de4b43
Bilal Haidar <bhaidar@gmail.com>
2019-09-27 11:51:32 +0300

    initial commit

---
+++ notes-create/index.js
@@ -0,0 +1,29 @@
+const faunadb = require('faunadb');
+
+const q = faunadb.query;
+const client = new faunadb.Client({ secret: 'fnADZPpx4EACAGpcBmat4T-ZHYRIdX607Upcve3x' });
+
+module.exports = async function (context, req) {
+    /* parse the string body into an actual note object */
+    const data = JSON.parse(context.body);
+    context.log('Function `notes-create` is invoked', data);
+
+    const notesDocument = { data };
+
+    /* create and execute a faundb query to create a new note */
+    return client.query(q.Create(q.Collection("Notes"), notesDocument))
+        .then(response => {
+            context.log('Note document created', response);
+            context.res = {
+                status: 200,
+                body: JSON.stringify(response)
+            };
+        })
+        .catch(error => {
+            context.log('Note document cannot be created', error);
+            context.res = {
+                status: 400,
+                body: JSON.stringify(response)
+            }
+        });
+};
